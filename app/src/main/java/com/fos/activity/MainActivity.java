2ee4a5fab6cee009607d52a756ee3354c3823052
zys <592813685@qq.com>
2018-04-10 00:15:43 +0800

    add CURD operations about databases

--- app/src/main/java/com/fos/activity/MainActivity.java
+++ app/src/main/java/com/fos/activity/MainActivity.java
@@ -2,16 +2,44 @@ package com.fos.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.fos.R;
+import com.fos.dao.DataInfoDao;
+import com.fos.database.DataInfo;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private DataInfoDao dataInfoDao=DataInfoDao.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int a=1;
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataInfo data = new DataInfo();
+                data.setAirHumidity(1);
+                data.setId(1);
+                data.setDate("sdf");
+                data.setSoilHumidity(1);
+                data.setLux(1);
+                data.setTemperature(1);
+                data.setWaterWeight(100);
+//              dataInfoDao.save(data);
+//              dataInfoDao.updateData(data);
+//              data = dataInfoDao.findFirstData();
+//              Toast.makeText(MainActivity.this,""+data,Toast.LENGTH_SHORT).show();
+//              dataInfoDao.delete();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/fos/util/LogUtil.java
@@ -0,0 +1,46 @@
+package com.fos.util;
+
+import android.util.Log;
+
+/**
+ * Author: 曾勇胜
+ * Date: 2018/4/9 10:26
+ * Email: 592813685@qq.com
+ * Description: 日志工具类
+ **/
+public final class LogUtil {
+
+    private static boolean isPrint=true;
+
+    private LogUtil(){}
+
+    public static void e(String tag,String info){
+        if (isPrint==true){
+            Log.e(tag, info);
+        }
+    }
+
+    public static void i(String tag,String info){
+        if (isPrint==true){
+            Log.i(tag, info);
+        }
+    }
+
+    public static void v(String tag,String info){
+        if (isPrint==true){
+            Log.v(tag, info);
+        }
+    }
+
+    public static void w(String tag,String info){
+        if (isPrint==true){
+            Log.w(tag, info);
+        }
+    }
+
+    public static void d(String tag,String info){
+        if (isPrint==true){
+            Log.d(tag, info);
+        }
+    }
+}
