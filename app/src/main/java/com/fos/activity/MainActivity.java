d830aee11663abbd24c88445ecc835619adff5e0
JustDoItSW <33753758+JustDoItSW@users.noreply.github.com>
2018-04-10 01:04:02 +0800

    Update MainActivity.java

--- app/src/main/java/com/fos/activity/MainActivity.java
+++ app/src/main/java/com/fos/activity/MainActivity.java
@@ -20,26 +20,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DataInfo data = new DataInfo();
-                data.setAirHumidity(1);
-                data.setId(1);
-                data.setDate("sdf");
-                data.setSoilHumidity(1);
-                data.setLux(1);
-                data.setTemperature(1);
-                data.setWaterWeight(100);
-//              dataInfoDao.save(data);
-//              dataInfoDao.updateData(data);
-//              data = dataInfoDao.findFirstData();
-//              Toast.makeText(MainActivity.this,""+data,Toast.LENGTH_SHORT).show();
-//              dataInfoDao.delete();
-            }
-        });
     }
 }
