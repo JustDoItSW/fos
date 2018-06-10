9860786269a7630e20ab0a0d1604415467dfa04f
JustDoItSW <592813685@qq.com>
2018-06-10 15:26:14 +0800

    添加语音识别功能（科大讯飞用）

--- app/src/main/java/com/fos/activity/SelectFlower.java
+++ app/src/main/java/com/fos/activity/SelectFlower.java
@@ -39,11 +39,25 @@ import com.fos.util.AudioRecoderUtils;
 import com.fos.util.InfomationAnalysis;
 import com.fos.util.MyListViewAdapter;
 import com.fos.util.TimeUtils;
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.resource.Resource;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 public class SelectFlower extends AppCompatActivity {
 
@@ -58,12 +72,15 @@ public class SelectFlower extends AppCompatActivity {
     private Map<String,Object> item;//数据项;
     private MyListViewAdapter myListViewAdapter;
     public static Handler handler;
-    private Button btn_record;
+    private Button btn_record,btn_voice;
     private AudioRecoderUtils audioRecoderUtils;
     private PopupWindowFactory  popupWindowFactory;
     private LinearLayout l1;
+    private SpeechRecognizer mIat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mIat=SpeechRecognizer.createRecognizer(SelectFlower.this,mInitListener);
+        SpeechUtility.createUtility( this, SpeechConstant.APPID+"=5b1cb818"); //+SpeechConstant.FORCE_LOGIN+"=true"
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_flower);
         init();
@@ -83,6 +100,8 @@ public class SelectFlower extends AppCompatActivity {
         delSearch_select =(ImageView)findViewById(R.id.delSearch_select);
         edit_search_select = (EditText)findViewById(R.id.edit_search_select);
         btn_record  =(Button)findViewById(R.id.btn_record);
+        btn_voice =findViewById(R.id.btn_voice);
+        btn_voice.setOnClickListener(OnClickListener);
         audioRecoderUtils = new AudioRecoderUtils();
         View view = View.inflate(SelectFlower.this, R.layout.layout_microphone, null);
         popupWindowFactory = new PopupWindowFactory(this,view);
@@ -294,6 +313,117 @@ public class SelectFlower extends AppCompatActivity {
             return true;
         }
     };
+    /**
+     * 录音的点击事件
+     */
+    View.OnClickListener OnClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if(edit_search_select.getText().toString().length()!=0)
+                edit_search_select.setText("");
+            setParam();
+            int ret =mIat.startListening(mRecognizerListener);
+            if(ret!= ErrorCode.SUCCESS){
+                Log.e("big11","识别失败，错误码："+ret);
+            }
+        }
+    };
+
+    /**
+     * 初始化监听器。
+     */
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            Log.d("11", "SpeechRecognizer init() code = " + code);
+        }
+    };
+
+    /**
+     * 参数设置
+     * @return
+     */
+    public void setParam(){
+        if(mIat==null)
+            mIat=SpeechRecognizer.createRecognizer(this,null);
+        mIat.setParameter(SpeechConstant.DOMAIN, "iat");
+        // 设置语言
+        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        // 设置语言区域
+        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+    }
+
+    /**
+     * 听写监听器。
+     */
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onBeginOfSpeech() {
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            // Tips：
+            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
+            if(error.getErrorCode() == 14002) {
+                Log.e("11",error.getPlainDescription(true)+"\n请确认是否已开通翻译功能" );
+            } else {
+                Log.e("11error",error.getPlainDescription(true));
+            }
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+        }
+
+        @Override
+        public void onResult(RecognizerResult results, boolean isLast) {
+            Log.d("result11文字", results.getResultString());
+            printResult(results);
+        }
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+        }
+    };
+
+    //回调结果：
+    private void printResult(RecognizerResult results) {
+        String text = parseIatResult(results.getResultString());
+        // 自动填写地址
+        Log.e("111",text);
+        edit_search_select.append(text);
+    }
+
+    /**
+     *  解析数据用
+     * @param json
+     * @return 解析好的数据
+     */
+    public static String parseIatResult(String json) {
+        StringBuffer ret = new StringBuffer();
+        try {
+            JSONTokener tokener = new JSONTokener(json);
+            JSONObject joResult = new JSONObject(tokener);
+
+            JSONArray words = joResult.getJSONArray("ws");
+            for (int i = 0; i < words.length(); i++) {
+                // 转写结果词，默认使用第一个结果
+                JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+                JSONObject obj = items.getJSONObject(0);
+                ret.append(obj.getString("w"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret.toString();
+    }
 
     AudioRecoderUtils.OnAudioStatusUpdateListener onAudioStatusUpdateListener = new AudioRecoderUtils.OnAudioStatusUpdateListener() {
         @Override
